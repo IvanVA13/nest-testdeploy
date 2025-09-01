e41a3796b4a9582e9a9c0226e6151e564f56c701
Ivan Volobuev <ivannnn2425@gmail.com>
2025-09-01 20:42:54 +0300

    fix: add dinamic port host

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
+const PORT = process.env.PORT || 3000;
+const HOST = '0.0.0.0';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableShutdownHooks()
-  await app.listen(3000)
+  await app.listen(PORT, HOST)
 }
 bootstrap()
