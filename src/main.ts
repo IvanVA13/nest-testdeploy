91b94766e2008fe39eb08ff2db2745c6a51219d6
Ivan Volobuev <71542875+IvanVA13@users.noreply.github.com>
2025-09-01 22:37:45 +0300

    Update2 main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ const port = process.env.PORT || 4000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableShutdownHooks()
-  await app.listen(PORT)
+  await app.listen(port)
 }
 bootstrap()
