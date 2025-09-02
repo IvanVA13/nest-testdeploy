217285ddd84f789f7e8ab66b9203c3b6e2161ad3
Ivan Volobuev <71542875+IvanVA13@users.noreply.github.com>
2025-09-02 16:06:07 +0300

    Update4 main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,6 @@ const port = Number.parseInt(process.env.PORT) || 4000
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableShutdownHooks()
-  await app.listen(port)
+  await app.listen(port, '0.0.0.0')
 }
 bootstrap()
